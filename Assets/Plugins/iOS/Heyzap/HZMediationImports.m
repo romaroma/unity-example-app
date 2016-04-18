660ddca996d87e13bcb4268cd9a6d5bd4eabbaf4
Roman Malakhov <romaroma@zoom-group.ru>
2016-04-18 16:16:00 +0600

    HZ manual file update

--- Assets/Plugins/iOS/Heyzap/HZMediationImports.m
+++ Assets/Plugins/iOS/Heyzap/HZMediationImports.m
@@ -2,19 +2,27 @@
 // This file was generated from Heyzap's Unity docs page http://developers.heyzap.com/docs/unity_sdk_setup_and_requirements
 // It imports the frameworks that each of the 3rd-party SDKs need.
 
+@import AVFoundation;
 @import AdSupport;
+@import AudioToolbox;
+@import CFNetwork;
 @import CoreGraphics;
 @import CoreLocation;
+@import CoreMedia;
 @import CoreTelephony;
 @import EventKit;
 @import EventKitUI;
+@import Foundation;
 @import MediaPlayer;
 @import MessageUI;
 @import MobileCoreServices;
 @import QuartzCore;
 @import Security;
+@import Social;
 @import StoreKit;
 @import SystemConfiguration;
+@import UIKit;
+@import WebKit;
 @import iAd;
 
 @interface HZMediationImports : NSObject
