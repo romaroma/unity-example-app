9a6aa796f8e0eff9572582eff9bfc1ed71c22777
Roman Malakhov <romaroma@zoom-group.ru>
2016-04-19 17:08:49 +0600

    Game center test

---
+++ Assets/Scripts/GameCenter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+public class GameCenter : MonoBehaviour {
+
+    public void authCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("Authentication successful");
+            string userInfo = "Username: " + Social.localUser.userName +
+                "\nUser ID: " + Social.localUser.id +
+                "\nIsUnderage: " + Social.localUser.underage;
+            Debug.Log(userInfo);
+        }
+        else
+        {
+            Debug.Log("Authentication failed");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Social.localUser.Authenticate(authCallback);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
