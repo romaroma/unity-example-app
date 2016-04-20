4f879d663da649bc410dda1fe9edc28ecad18e75
Roman Malakhov <romaroma@zoom-group.ru>
2016-04-20 21:38:38 +0600

    Test purchases

--- Assets/Scripts/Purchaser.cs
+++ Assets/Scripts/Purchaser.cs
@@ -1,13 +1,131 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using UnityEngine.Purchasing;
 
-public class Purchaser : MonoBehaviour {
+public class Purchaser : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController; // Reference to the Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider;
+    private string internalProdId = "skipAds";
+    private string itunesProdId = "Remove Ads and Save Levels";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public void BuyProduct()
+    {
+        var productId = internalProdId;
+        // If the stores throw an unexpected exception, use try..catch to protect my logic here.
+        try
+        {
+            // If Purchasing has been initialized ...
+            if (IsInitialized())
+            {
+                // ... look up the Product reference with the general product identifier and the Purchasing system's products collection.
+                Product product = m_StoreController.products.WithID(productId);
+
+                // If the look up found a product for this device's store and that product is ready to be sold ... 
+                if (product != null && product.availableToPurchase)
+                {
+                    Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));// ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed asynchronously.
+                    m_StoreController.InitiatePurchase(product);
+                }
+                // Otherwise ...
+                else
+                {
+                    // ... report the product look-up failure situation  
+                    Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+                }
+            }
+            // Otherwise ...
+            else
+            {
+                // ... report the fact Purchasing has not succeeded initializing yet. Consider waiting longer or retrying initiailization.
+                Debug.Log("BuyProductID FAIL. Not initialized.");
+            }
+        }
+        // Complete the unexpected exception handling ...
+        catch (Exception e)
+        {
+            // ... by reporting any unexpected exception for later diagnosis.
+            Debug.Log("BuyProductID: FAIL. Exception during purchase. " + e);
+        }
+    }
+
+    public void InitializePurchasing()
+    {
+        // If we have already connected to Purchasing ...
+        if (IsInitialized())
+        {
+            // ... we are done here.
+            return;
+        }
+
+        // Create a builder, first passing in a suite of Unity provided stores.
+        var module = StandardPurchasingModule.Instance();
+        var builder = ConfigurationBuilder.Instance(module);
+
+        // Add a product to sell / restore by way of its identifier, associating the general identifier with its store-specific identifiers.
+        builder.AddProduct(internalProdId, ProductType.NonConsumable, new IDs() { { itunesProdId, AppleAppStore.Name }});
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    private bool IsInitialized()
+    {
+        // Only say we are initialized if both the Purchasing references are set.
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+    //  
+    // --- IStoreListener
+    //
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        // Purchasing has succeeded initializing. Collect our Purchasing references.
+        Debug.Log("OnInitialized: PASS");
+
+        // Overall Purchasing system, configured with products for this application.
+        m_StoreController = controller;
+        // Store specific subsystem, for accessing device-specific store features.
+        m_StoreExtensionProvider = extensions;
+    }
+
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        // Purchasing set-up has not succeeded. Check error for reason. Consider sharing this reason with the user.
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        // A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing this reason with the user.
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        // A non-consumable product has been purchased by this user.
+        if (args.purchasedProduct.definition.id == internalProdId)
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+        }
+        else
+        {
+            Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+        }// Return a flag indicating wither this product has completely been received, or if the application needs to be reminded of this purchase at next app launch. Is useful when saving purchased products to the cloud, and when that save is delayed.
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+    // Use this for initialization
+    void Start () {
+        // If we haven't set up the Unity Purchasing reference
+        if (m_StoreController == null)
+        {
+            // Begin to configure our connection to Purchasing
+            InitializePurchasing();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
