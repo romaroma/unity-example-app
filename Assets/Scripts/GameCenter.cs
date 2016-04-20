6386b352333bec2fbeed94fe31a63216551a5ab9
Roman Malakhov <romaroma@zoom-group.ru>
2016-04-20 12:11:07 +0600

    Game center stub

--- Assets/Scripts/GameCenter.cs
+++ Assets/Scripts/GameCenter.cs
@@ -3,20 +3,42 @@ using System.Collections;
 using UnityEngine.SocialPlatforms.GameCenter;
 
 public class GameCenter : MonoBehaviour {
+    bool isGameCenterAuth = false;
+    long score = 3; // number of levels completed
+    public ScrollingTextArea console;
 
     public void authCallback(bool success)
     {
         if (success)
         {
             Debug.Log("Authentication successful");
+            this.console.Append("Authentication successful");
             string userInfo = "Username: " + Social.localUser.userName +
                 "\nUser ID: " + Social.localUser.id +
                 "\nIsUnderage: " + Social.localUser.underage;
             Debug.Log(userInfo);
+            this.console.Append(userInfo);
+
+            isGameCenterAuth = true;
         }
         else
         {
             Debug.Log("Authentication failed");
+            this.console.Append("Authentication failed");
+        }
+    }
+
+    public void scoreSubmissionCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("Score submission success");
+            this.console.Append("Score submission success");
+        }
+        else
+        {
+            Debug.Log("Score submission failed");
+            this.console.Append("Score submission failed");
         }
     }
 
@@ -27,6 +49,15 @@ public class GameCenter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        // send one result to leaderboard
+	    if (isGameCenterAuth)
+        {
+            // what a trick!
+            isGameCenterAuth = false;
+            // submit a score
+            Social.ReportScore(score, "T001", scoreSubmissionCallback);
+            // show leaderboard
+            Social.ShowLeaderboardUI();
+        }
 	}
 }
